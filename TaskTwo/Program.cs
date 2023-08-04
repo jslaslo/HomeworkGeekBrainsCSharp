a7f5ca4010923792612650a59ab999f92df9ccb6
dergousov.vladislav <dergousov95@yandex.ru>
2023-08-04 19:28:34 +0300

    add changes

--- TaskTwo/Program.cs
+++ TaskTwo/Program.cs
@@ -7,14 +7,14 @@ int secondValue = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int thirdValue = int.Parse(Console.ReadLine());
 
-System.Console.WriteLine($"Max = {Max(firstValue, secondValue, thirdValue)}");
+Console.WriteLine($"Max = {Max(firstValue, secondValue, thirdValue)}");
 
 int Max(int firstValue, int secondValue, int thirdValue){
-    if(firstValue > secondValue && secondValue>thirdValue){
+    if(firstValue >= secondValue && secondValue>thirdValue){
         int max = firstValue;
         return max;
     }
-    else if (secondValue > firstValue && firstValue > thirdValue){
+    else if (secondValue >= firstValue && firstValue > thirdValue){
         int max = secondValue;
         return max;
     }
